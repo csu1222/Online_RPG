972b040e509d2569d7f7a30af383cef8b83736c3
seonguk <cjstjddnr12@gmail.com>
2024-07-17 16:11:50 +0900

    GameServer 프로젝트에서 Future, Promise, pakaged_task

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -4,118 +4,140 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
-#include <windows.h>
+// 추가
+#include <future>
 
-// Condition Variable
+// Future
 
 /*
-이전 이벤트 실습에서 사실 불안전한 부분이 있습니다. 
-실습에서 Producer 함수는 10000ms 라는 시간마다 일감을 밀어넣었는데 이 속도는 너무 인위적인 속도이고 
-실제 서비스에서는 한번에 일감이 몰릴 수 도 있고 일감이 한가할 때도 있습니다. 
-만약 일감이 너무 많이 몰려서 사실상 매 프레임마다 q.push 를 해야하는 상황이라고 하겠습니다. 
-그러면 생각하기로는 SetEvent도 매 프레임 호출이 되어서 Consumer 도 매 프레임 일감을 처리해주면서 적어도 0~1개 사이의 일감들만 
-남아 있을것 같습니다. 
+조건변수의 경우는 Queue에 일감을 밀어넣고 그 일감을 꺼내 처리하는 작업에 아주 잘 어울리는 클래스였습니다. 
 
-하지만 실제로 Consumer 에서 q.size() 를 출력하도록 하고 디버깅을 실행하면 q의 사이즈가 빠른 속도로 증가한다는걸 볼 수 있습니다. 
-어떻게 보면 Event 로 쓰레드를 깨우는 부분과 락을 걸고 q.push, q.pop 을 하는 부분이 묶여있지 않기 때문에 멀티쓰레드 환경에서의 순서 틀어짐이 
-발생할 수 있습니다. 
+조금 더 가벼운 작업에 사용할 수 있는 Future 클래스에 대해 알아볼겁니다. 
 
-Producer 에서 SetEvent 로 signal을 주고 Consumer에서 WaitSingleObject에 들어와 쓰레드과 활성화 되는 순간과 이어서 락을 잡는 이 사이에는 
-다른 멀티쓰레드가 개입할 여지가 있습니다. 그래서 이 사이에 다시 Producer 가 동작해 먼저 락을 잡고 push 를 할 수도 있습니다. 
+먼저 동기화 방식이라는 것을 알아야 하는데 동기화 방식이라는것는 프로그램이 진행되면서 처리되는 코드들이 있을겁니다. 
+이 코드의 진행이 이 쓰레드 안에서 단 한가지 뿐이라 함수가 호출된다면 그 함수의 모든 코드를 처리하고 나서 나와야 다시 원래의 진행 순서로 
+가는것입니다. 
+원래 처음 배우는 프로그래밍에서 동기화 방식은 아주 당연해 보입니다. 
+하지만 어떤 함수의 처리가 100초나 걸리는 함수였다면 함수가 처리 될 때까지 코드 진행이 멈추기도 할겁니다. 
 
-물론 p.pop 을 할때 if 문이 아니라 while 문으로 한번 들어오면 q 안에 있는 모든 일감을 꺼낸다거나 하는 식으로 처리 할 수도 있지만 
+비동기 방식으로 처리를 하면 오래걸리는 함수를 병렬적으로 진행하고 이어서 코드를 진행 할 수 있을겁니다. 
+배운 내용중에서는 쓰레드를 하나 만들어 오래걸리는 함수를 시키고 메인 쓰레드는 나름의 코드를 진행하는것일겁니다. 
 
-중요한 점은 락과 이벤트는 별개라는 것입니다. 
+하지만 일꾼 쓰레드의 함수가 끝나 join() 까지 도달하면 쓰레드 내부의 데이터는 다 날라갔을 테니 
+따로 전역 변수 같은걸 만들어 여기에 저장하는것까지 해야 그 결과물을 알 수 있을겁니다. 
+그래도 공용데이터를 다룬다는것 자체가 불편합니다. 락을 걸어야 할 수도 있기 때문이도 또 쓰레드들이 하나만 있을것이 아니고 
+그 안에서 필요한 정보들도 한둘이 아닐텐데 각각 공용데이터로 변수를 만들어 받아줘야하는것도 실수위험이나 가독성에 문제가 있습니다. 
+
+거기에 쓰레드를 새로 하나 만드는 동작은 생각보다 무거운 작업이기 때문에 좀 더 오래 사용할 일감에 해주는게 좋고 
+간단하게 단기 알바 같은 일감은 이번 시간 주제인 Future 객체를 사용하면 좋겠습니다. 
+
+future 객체를 사용하는 방법은 세가지가 있는데 std::async 로 future 객체를 생성하는 방법(async, deffered 두가지 옵션이 있습니다.)
+그리고 promise 객체와 future 객체를 연동하고 promise 객체를 다른 쓰레드에서 실행한후 그 값을 future 객체에서 가져오는경우
+그리고 pakaged_task를 만들때 실행할 함수를 인자로 주고 다른 쓰레드에서 그 pakaged_task를 처리하는 함수와 pakaged_task 객체를 
+같이 넘겨주고 나중에 future 객체에서 값을 가져오는 방법이 있습니다.
+promise 와 너무 비슷해보이지만 이 pakaged_task 는 사실 쓰레드를 만들면서 꼭 하나의 pakaged_task를 넘겨야하는게 아닌 여러 객체를 넘겨서 
+한번에 처리하도록 할 수도 있습니다. 
 
 
-이번 주제인 Condition Variable 입니다. 
-표준으로 들어온 개념이기 때문에 곧바로 condition_variable 타입으로 사용할 수 있습니다. mutex에 포함된 클래스입니다. 
-그렇다 보니 condition_variable 은 아무데나 사용할 수 없고 mutex와 병용해야 합니다. 
-만약 자유롭게 사용하고 십다면 condition_variable 라이브러리를 include 하고 condition_variable_any 타입을 사용해야합니다.  
-
-Condition Variable 조건변수는 커널 오브젝트가 아닌 유저레벨 오브젝트입니다. 
-
-조건변수는 이벤트와 거의 비슷하게 동작을 하는데 다른점은 락과 짝을지어서 동작해야합니다. 
-
-조건변수를 사용할때 notify를 보내는 쪽은 락을 잡고 코드를 진행한 후 락을 푼다음 notify를 합니다.
-notify를 받는 쪽은 락을 먼저 걸고 cv.wait를 호출하는데 사용한 락과 동작할 조건을 함수로 줍니다. 
-조건이 참이라면 이어서 코드를 실행하고 조건이 거짓이라면 락을 풀고 이 쓰레드는 대기상태로 갑니다. 
-
-이때 락을 잠그고 풀어야 하기때문에 lock_guard 대신 unique_lock 을 사용합니다. 
-
-wait 의 조건을 통과해야 다음 코드가 실행되기때문에 이어서 코드에서 if나 while 문으로 다시 조건을 판별하지 않아도 됩니다. 
-
-생각해보면 notify_one 을 호출하는것 자체가 wait 에서 깨어날 조건에 부합한다면 코드를 진행하기 때문에 굳이 wait 에서 일어날 조건을 걸지 않아도 될것 같습니다. 
-이런상황을 Spurious WakeUp 입니다. 공유 변수값을 수정하는것과 notify_one을 호출하는 것은 분리 되어 있기 때문에 이 사이에 다른 어떤 쓰레드에서 
-공유 변수를 수정해 데이터를 비워버린다던가 내용물을 바꿔치기 한다면 cv.wait 로 깨어났지만 처리할 데이터가 없다고 나올 수 있습니다. 
-
-또 한가지 궁금함은 notify_one 을 호출할때 락을 풀고 호출하는데 꼭 그래야 하는가 입니다. 
-락을 잡은 상태에서 notify_one을 호출할 수 있기는하지만 권장되지 않는 방법입니다. 
-
-락이 걸린상태에서 notify_one을 호출하면 어느쓰레드의 cv.wait가 깨어나 락을 잡고 조건을 확인하려고 하는데 이미 락이 잡힌 상태라서 제대로 동작하지 않습니다. 
+이 모든 방법 또는 future 객체를 꼭 사용해야하는것은 아니지만 일단 이런 개념이 있다라고 알고 넘어가는겁니다.  
 */
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
-
-// 표준으로 사용할 수 있습니다.	mutex 라이브러리에 포함되어 mutex와 함깨 사용해야함
-condition_variable cv;
-
-// 자유롭게 사용하려면 이렇게 
-//#include <condition_variable>
-//condition_variable_any cva;
-
-void Producer()
+int64 Calculate()
 {
-	while (true)
-	{	
-		// 1) 락을 잡고
-		// 2) 공유 변수 값을 수정
-		// 3) 락을 풀고
-		// 4) 조건변수를 통해 다른 쓰레드에게 통지
+	int64 sum = 0;
+	
+	for (int32 i = 0; i < 100'000; i++)
+		sum += i;
 
-		{
-			unique_lock<mutex> lock(m);
-
-			q.push(100);
-		}
-
-		cv.notify_one();	// wait 중인 쓰레드가 있으면 딱 한개를 깨운다
-	}
+	return sum;
 }
 
-void Consumer()
+void PromiseWorker(std::promise<string>&& promise)
 {
-	while (true)
-	{
-		// 1) 락을 잡고
-		// 2) 조건변수를 통해 wait를 합니다. 이때 탈출조건을 함수로 줄 수 있습니다. 
-		// 2-1) 만약 조건을 만족한다 -> wait에서 빠져나와 이어서 코드를 진행 
-		// 2-2) 조건을 만족하지 않는다 -> 락을 풀어주고 대기상태로 전환합니다. 
-		//  이때 락을 풀어주어야 하기 때문에 락가드를 사용하지 않고 유니크락을 사용함
+	promise.set_value("Set Promise Massage");
+}
 
-		unique_lock<mutex> lock(m);
-		cv.wait(lock, []() { return q.empty() == false; });
-
-		//while (q.empty() == false)		// cv.wait 에서 이미 조건을 체크하기 때문에 다시 조건을 체크할 필요가 없습니다. 
-		{
-			int32 item = q.front();
-			q.pop();
-
-			cout << item << endl;
-		}
-	}
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
 }
 
 int main()
 {
+	// 동기(Synchronous) 실행
+	int64 sum = Calculate();
+	cout << sum << endl;
+
+	
 	{
-		thread t1(Producer);
-		thread t2(Consumer);
+		// Future 객체를 생성하는 방법 
+		// 인자의 뜻 async(비동기화)로 Calculate 를 실행해달라 
+		// 첫번째 인자로 3가지 옵션을 줄 수 있습니다. 
+		// 1) async				: 별도의 쓰레드를 만들어 실행하세요 
+		// 2) deffered			: lazy evaluation 지연해서 실행하세요 
+		// 3) deffered | async	: 둘 중 알아서 골라주세요 
+		std::future<int64> future = std::async(std::launch::async, Calculate);
 
-		t1.join();
-		t2.join();
+		// TODO : future 객체를 만든 후 당장 그 값이 필요하지 않다면 다른 작업들을 진행합니다. 
 
+		// 한참 후에 future 값이 필요해진다면 
+		int64 sum = future.get(); 
+		// async 로 만들었다면 별도의 쓰레드에서 계산해둔 값을 가져오는것 
+		// deffered 로 만들었다면 사실은 int64 sum = Calculate(); 와 다르지 않습니다. 
+
+		// deffered 는 그냥 함수호출을 지연시키는건데 어떤 의미가 있을까?
+		// 커맨트 패턴에서 필요한 경우가 생깁니다.
+		// 한번에 일감이 몰려서 일감들을 미뤄둬야할때 지금처럼 지연시켜서 나중에 실행하도록 할 수 있습니다. 
+
+		// 경우에 따라서 future의 진행 상황을 알고 싶을때가 있습니다. 
+		// 그럴때 future 객체의 wait_for 를 하게 되면 인자에 준 시간이 흐르고 나서 future_status 를 반환합니다.   
+		std::future_status status = future.wait_for(1ms);
+		if (status == std::future_status::ready)
+		{
+
+		}
+		// wait_for 말고 그냥 wait 는 무한정으로 기다린다는 것입니다. 사실상 .get() 과 별 차이가 없습니다. 
+
+
+		// 추가로 future 객체에 넘겨줄 함수를 그냥 함수가 아니라 클래스에 포함된 멤버함수를 넘겨줄 때도 있을겁니다. 
+		{
+			class Knight
+			{
+			public:
+				int64 GetHp() { return 100; }
+			};
+
+			// 객체의 멤버 함수는 객체에 포함된 데이터 이다 보니 객체가 있고 그걸 같이 넘겨줘야합니다. 
+			Knight knight;
+			std::future<int64> future2 = std::async(std::launch::async, Knight::GetHp, knight);
+		}
+
+	}
+
+	// future 객체가 꼭 async 를 통해만 만들수 있는건 아닙니다. 
+	// std::promise
+	{
+		// 프로미스 객체를 만들고 퓨처객체와 get_future 함수를 통해 둘을 연동 시킬 수 있습니다. 
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
+
+		// 이후 promise에 Set 함수를 통해 갑을 세팅해주면 나중에 future.get()으로 그 값을 가져올 수 있습니다. 
+		thread t(PromiseWorker, std::move(promise));
+
+		string msg = future.get();
+		cout << msg << endl;
+		t.join();
+	}
+
+	// std::pakaged_task
+	{
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future = task.get_future();
+
+		thread t(TaskWorker, std::move(task));
+		
+		int64 sum = future.get();
+		cout << sum << endl;
+		t.join();
 	}
 }
