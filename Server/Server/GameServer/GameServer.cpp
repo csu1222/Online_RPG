b58850243840d3b6d72fde5b3368219e528021ed
seonguk <cjstjddnr12@gmail.com>
2024-07-27 21:05:23 +0900

    참조 카운트 개념 텍스트파일로

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -8,33 +8,7 @@
 #include <future>
 #include "ThreadManager.h"
 
-// 테스트 
-#include "AccountManager.h"
-#include "PlayerManager.h"
 
 int main()
 {
-	GThreadManager->Launch([=]()
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerManager.PlayerThenAccount();
-
-				this_thread::sleep_for(100ms);
-			}
-		});
-
-	GThreadManager->Launch([=]()
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer" << endl;
-				GAccountManager.AccountThenPlayer();
-
-				this_thread::sleep_for(100ms);
-			}
-		});
-
-	GThreadManager->Join();
 }
