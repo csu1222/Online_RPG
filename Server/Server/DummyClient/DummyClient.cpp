56500ec570e10b7064be6f543195257f477c4adb
seonguk <cjstjddnr12@gmail.com>
2024-08-08 21:58:15 +0900

    DummyClient, GameServer 에서 소켓 프로그래밍 개론

--- Server/Server/DummyClient/DummyClient.cpp
+++ Server/Server/DummyClient/DummyClient.cpp
@@ -2,6 +2,95 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include "winsock2.h"
+#include "MSWSock.h"
+#include "WS2tcpip.h"
+#pragma comment(lib, "ws2_32.lib")
+
 int main()
 {
+	// winsock 라이브러리 초기화  (ws2_32 라이브러리 초기화)
+	// WORD 타입 윈속 버전과 윈속을 초기화 한 후의 데이터를 받아줄 WSAData 의 주소 
+	WORD wVersionRequestes;
+	WSAData wsaData;
+
+	int err;
+
+	wVersionRequestes = MAKEWORD(2, 2);
+	err = ::WSAStartup(wVersionRequestes, &wsaData);
+
+	// err 가 0 이면 성공 아니면 실패
+	if (err != 0)
+	{
+		/* Tell the user that we could not find a usable */
+		/* Winsock DLL.                                  */
+		printf("WSAStartup failed with error: %d\n", err);
+		return 1;
+	}
+
+	// wsaData.wVersion 의 상위 바이트는 부버전 하위 바이트는 주버전을 뜻함 
+	// 아래 코드는 winsock 버전이 2.2 가 아닌경우의 예외 처리를 하고 있습니다. 
+	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+		/* Tell the user that we could not find a usable */
+		/* WinSock DLL.                                  */
+		printf("Could not find a usable version of Winsock.dll\n");
+		WSACleanup();
+		return 1;
+	}
+	else
+		printf("The Winsock 2.2 dll was found okay\n");
+
+	// TODO
+	// 클라이언트 소켓 만들기 
+	// 인자는 총 3개 
+	// int af : address family 주소 패밀리 주로 기억해야 하는 것은 AF_INET(IPv4), AF_INET6(IPv6)
+	// int type : 새 소켓의 형식 사양 TCP 프로토콜에 사용되는 SOCK_STREAM 혹은 UDP 에 사용되는 SOCK_DGRAM 을 주로 사용한다.
+	// int protocol : 사용할 프로토콜 사용할 통신의 세세한 종류에 따라 고를 수 있는데 0 값을 넣어주면 알아서 골라주게 됩니다. 
+	// 반환값은 socket descripter 
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+
+	// SOCKET 타입에 커서를 가져다 대면 UINT_PTR 입니다. 정수의 포인터인것인데
+	// 운영체제가 이 해당 번호에 해당하는 소켓리소스를 통해 네트워크 통신을 할 수 있게 됩니다. 
+
+	// 소켓을 생성하는데 실패한 예외 처리
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Create Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 접속할 서버의 주소를 기입
+	SOCKADDR_IN serverAddr; // IPv4 에서 사용되는 주소객체
+	::memset(&serverAddr, 0, sizeof(serverAddr));	// serverAddr를 0으로 밀기
+	serverAddr.sin_family = AF_INET;	// 사용할 주소체계
+	// serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");	// 접속할 주소 하지만 너무 오래되서 사용하는걸 권장하지 않는 방식 
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);	// 현대적인 방식
+	serverAddr.sin_port = ::htons(7777);	// 접속할 포트 번호 
+
+	// 정해진 주소로 연결시도 
+	// 인자는 순서대로 소켓, 주소 구조체, 주소구조체의 사이즈를 받습니다. 
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Connect ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 연결성공 이 후로 데이터 송수신 가능 
+
+	cout << "Connecte To Server" << endl;
+
+	while (true)
+	{
+		// TODO
+
+		this_thread::sleep_for(10ms);
+	} 
+	
+	// 소켓 리소스 반환 
+	::closesocket(clientSocket);
+	
+	// winsock 종료
+	::WSACleanup();
 }
--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -10,52 +10,101 @@
 #include "Memory.h"
 #include "Allocator.h"
 
-class Knight
-{
-public:
-	int32 _hp = rand() % 1000;
-};
-
-class Monster
-{
-public:
-	int64 _id = 0;
-};
+#include "winsock2.h"
+#include "MSWSock.h"
+#include "WS2tcpip.h"
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	Knight* knights[100];
+	// winsock 초기화는 공통적인 부분
+	WORD wVersionRequestes;
+	WSAData wsaData;
 
-	for (int32 i = 0; i < 100; i++)
+	int err;
+
+	wVersionRequestes = MAKEWORD(2, 2);
+	err = ::WSAStartup(wVersionRequestes, &wsaData);
+
+	// err 가 0 이면 성공 아니면 실패
+	if (err != 0)
 	{
-		knights[i] = ObjectPool<Knight>::Pop();
+		printf("WSAStartup failed with error: %d\n", err);
+		return 1;
 	}
 
-	for (int32 i = 0; i < 100; i++)
+	// wsaData.wVersion 의 상위 바이트는 부버전 하위 바이트는 주버전을 뜻함 
+	// 아래 코드는 winsock 버전이 2.2 가 아닌경우의 예외 처리를 하고 있습니다. 
+	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+		printf("Could not find a usable version of Winsock.dll\n");
+		WSACleanup();
+		return 1;
+	}
+	else
+		printf("The Winsock 2.2 dll was found okay\n");
+
+
+	// 서버의 소켓 만들기 
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+
+	// 소켓을 생성하는데 실패한 예외 처리
+	if (listenSocket == INVALID_SOCKET)
 	{
-		ObjectPool<Knight>::Push(knights[i]);
-		knights[i] = nullptr;
+		int32 errCode = ::WSAGetLastError();
+		cout << "Create Socket ErrorCode : " << errCode << endl;
+		return 0;
 	}
 
-	shared_ptr<Knight> sptr = ObjectPool<Knight>::MakeShared();
+	// 서버는 연결할 목적지 주소가 아니라 서버 자신의 주소를 설정
+	SOCKADDR_IN serverAddr; // IPv4 에서 사용되는 주소객체
+	::memset(&serverAddr, 0, sizeof(serverAddr));	// serverAddr를 0으로 밀기
+	serverAddr.sin_family = AF_INET;	// 사용할 주소체계
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);	// 주소를 알아서 넣어달라 (고정주소를 넣어주면 딱 그 주소만 사용해야 하기 때문)
+	serverAddr.sin_port = ::htons(7777);	// 접속할 포트 번호 
 
-
-	for (int32 i = 0; i < 3; i++)
+	// listen socket 에 서버 주소를 주고 bind
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 	{
-		GThreadManager->Launch([]()
-			{
-				while (true)
-				{
-					Knight* knight = xnew<Knight>();
-
-					cout << knight->_hp << endl;
-
-					this_thread::sleep_for(10ms);
-
-					xdelete(knight);
-				}
-			});
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
 	}
 
-	GThreadManager->Join();
+	// 연결준비 완료 
+	// 인자로 소켓과 백로그라는 것을 받는데 백로그는 최대 연결 대기열 번호 
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 연결 성공 후 데이터 송수신 가능 
+
+	while (true)
+	{
+		// 클라이언트가 connect 요청을 했다면 그것을 승락하는 accept
+		
+		SOCKADDR_IN clientAddr;	// 연결시도한 클라이언트의 주소를 받아줄 구조체
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		// 클라이언트와 데이터를 주고 받을 소켓 
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		// 클라이언트 입장
+		// 클라이언트의 아이피를 찍어 보겠습니다. 
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! IP = " << ipAddress << endl;
+	}
+
+	// winsock 종료
+	::WSACleanup();
 }
