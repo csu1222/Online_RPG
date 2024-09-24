836c4516da859c5c42ac81b3c401c7dd7a5a822b
seonguk <cjstjddnr12@gmail.com>
2024-09-24 16:27:02 +0900

    PacketHandler 기초적인 패킷 직렬화

--- Server/Server/GameServer/GameSession.cpp
+++ Server/Server/GameServer/GameSession.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "GameSession.h"
 #include "GameSessionManager.h"
+#include "ServerPacketHandler.h"
 
 void GameSession::OnConnected()
 {
@@ -12,12 +13,10 @@ void GameSession::OnDisconnected()
 	GSessionManager.Remove(static_pointer_cast<GameSession>(shared_from_this()));
 }
 
-int32 GameSession::OnRecvPacket(BYTE* buffer, int32 len)
+void GameSession::OnRecvPacket(BYTE* buffer, int32 len)
 {
-	PacketHeader header = *((PacketHeader*)buffer);
-	cout << "Packet ID : " << header.id << "Size : " << header.size << endl;
+	ServerPacketHandler::HandlePacket(buffer, len);
 
-	return len;
 }
 
 void GameSession::OnSend(int32 len)
