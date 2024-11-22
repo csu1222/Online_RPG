ef97974a0e61f90ee0addc6a400f673d1f6cb376
seonguk <cjstjddnr12@gmail.com>
2024-11-22 18:56:41 +0900

    Procedure Generator

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -15,6 +15,7 @@
 #include "DBBind.h"
 #include "XmlParser.h"
 #include "DBSynchronizer.h"
+#include "GenProcedures.h"
 
 enum
 {
@@ -44,11 +45,42 @@ int main()
 
 	ASSERT_CRASH(GDBConnectionPool->Connect(1, L"Driver={ODBC Driver 18 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=ServerDB;Trusted_Connection=Yes;"));
 
-
 	DBConnection* dbConn = GDBConnectionPool->Pop();
 	DBSynchronizer dbSync(*dbConn);
 	dbSync.Synchronize(L"GameDB.xml");
 
+	{
+		WCHAR name[] = L"csu";
+
+		SP::InsertGold insertGold(*dbConn);
+		insertGold.In_Gold(100);
+		insertGold.In_Name(name);
+		insertGold.In_CreateDate(TIMESTAMP_STRUCT{ 2020, 6, 8 });
+		insertGold.Execute();
+	}
+
+	{
+		SP::GetGold getGold(*dbConn);
+		getGold.In_Gold(100);
+
+		int32 id = 0;
+		int32 gold = 0;
+		WCHAR name[100];
+		TIMESTAMP_STRUCT date;
+
+		getGold.Out_Id(OUT id);
+		getGold.Out_Gold(OUT gold);
+		getGold.Out_Name(OUT name);
+		getGold.Out_CreateDate(OUT date);
+
+		getGold.Execute();
+
+		while (getGold.Fetch())
+		{
+			GConsoleLogger->WriteStdOut(Color::BLUE,
+				L"ID[%d] Gold[%d] Name[%s]\n", id, gold, name);
+		}
+	}
 
 	ClientPacketHandler::Init();
 
--- Server/Server/Tools/PacketGenerator/MakeExe.bat
+++ Server/Server/Tools/PacketGenerator/MakeExe.bat
@@ -1,6 +1,7 @@
-pushd &~dp0
+pushd %~dp0
 pyinstaller --onefile PacketGenerator.py
 MOVE .\dist\PacketGenerator.exe .\GenPackets.exe
 @RD /S /Q .\build
 @RD /S /Q .\dist
 DEL /S /F /Q .\PacketGenerator.spec
+PAUSE
